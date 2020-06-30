81b638f6d7a060ecb07859ea2bb27a513af9c505
markschmucker <36678240+markschmucker@users.noreply.github.com>
2020-06-30 13:41:58 +0930

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from StringIO import StringIO
+from flask import Flask, request, send_file, redirect, render_template
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+@app.route('/media/get/<username>', methods=['GET'])
+def get_image(username):
+	data = 'x'
+	buff = StringIO()
+	buff.write(data)
+	buff.seek(0)
+	return send_file(buff, mimetype="image/jpeg")
+
+if __name__ == '__main__':
+	app.run()
